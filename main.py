ac199c8d9f833da248304b7b3c689fed5d58f4f5
PotatoPcDead <potatopcdead@gmail.com>
2023-07-18 15:46:35 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+from tkinter import *
+import random
+import pygame
+import os
+
+os.environ['SDL_VIDEO_CENTERED'] = "1"
+pygame.init()
+info = pygame.display.Info()
+
+WIDTH, HEIGHT = info.current_w, info.current_h
+class Snake:
+    pass
+
+class Food:
+    pass
+
+def next_turn():
+    pass
+
+def change_direction(new_direction):
+    pass
+
+def check_collisions():
+    pass
+
+def game_over():
+    pass
