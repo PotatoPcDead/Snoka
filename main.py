8fc2875353022808127eb14ab32eca01546f2585
PotatoPcDead <potatopcdead@gmail.com>
2023-07-18 16:13:01 +0500

    qwe

--- main.py
+++ main.py
@@ -5,11 +5,19 @@ import os
 
 os.environ['SDL_VIDEO_CENTERED'] = "1"
 pygame.init()
+# getting info
 info = pygame.display.Info()
-
+# setting screen width and height
 WIDTH, HEIGHT = info.current_w, info.current_h
+print(WIDTH, HEIGHT)
 SPEED = 50
 SPACE_SIZE = 50
+BODY_PARTS = 3
+SNAKE_COLOR = "#00FF00"
+FOOD_COLOR = "#FF0080"
+BACKGROUND_COLOR = "#000000"
+WHITE = "#FFFFFF"
+LIME = "#00FF00"
 class Snake:
     pass
 
@@ -27,3 +35,18 @@ def check_collisions():
 
 def game_over():
     pass
+
+window = Tk()
+window.title("Snek Game")
+window.resizable(True, True)
+
+score = 0
+direction = "down"
+
+label = Label(window, text = "Score:{}".format(score), font=("cursive", 40),  fg=LIME)
+label.pack()
+
+canvas = Canvas(window, bg=BACKGROUND_COLOR, height=HEIGHT, width=WIDTH)
+canvas.pack()
+
+window.mainloop()
